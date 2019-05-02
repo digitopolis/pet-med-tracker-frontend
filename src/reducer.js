0b745f6cecba70c9498ebd32279c0c5d08beac1a
Matt Readout <matt.readout@gmail.com>
2019-05-02 17:57:08 -0400

    setup redux store

---
+++ src/reducer.js
@@ -0,0 +1,15 @@
+const defaultState = {
+	users: []
+}
+
+const reducer = (state = defaultState, action) => {
+	switch (action.type) {
+		case 'FETCH_USERS':
+			return {...state,
+				users: action.payload}
+		default:
+			return state
+	}
+}
+
+export default reducer
