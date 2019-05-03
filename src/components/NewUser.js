40ef5bf88c8bae406502244825a2994b5e796583
Matt Readout <matt.readout@gmail.com>
2019-05-02 22:04:16 -0400

    create new user form

---
+++ src/components/NewUser.js
@@ -0,0 +1,45 @@
+import React from 'react'
+// import PropTypes from 'prop-types'
+
+class NewUser extends React.Component {
+
+	state = {
+		username: '',
+		password: '',
+		submitted: false
+	}
+
+	handleChange = (event) => {
+		const target = event.target
+		const value = target.value
+		const name = target.name
+
+		this.setState({ [name]: value })
+	}
+
+	handleSubmit = () => {
+		alert('submitted');
+	}
+
+
+	render () {
+		return(
+			<div>
+				<form onSubmit={this.handleSubmit}>
+					<label>
+						Username:
+						<input 
+							type='text'
+							name='username'
+							value={this.state.username}
+							onChange={this.handleChange}
+						/>
+					</label>
+					<input type='submit'/>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default NewUser;
